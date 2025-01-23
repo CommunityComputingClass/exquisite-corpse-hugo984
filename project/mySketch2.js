b3dfd8f1e5dd5e770412d02d4ff8856bef596ed6
hugo984 <26hugo.loeb@lwhs.org>
2025-01-23 17:53:02 +0000

    eyes

--- project/mySketch2.js
+++ project/mySketch2.js
@@ -4,11 +4,10 @@ let colors = ["red", "green", "yellow", "purple"];
 function setup() {
     createCanvas(500, 500);
     background("violet");
-    fill(255);
-    rect(0, 0, 75, 20);
+    
+    
     
     eyes(250,250);
-    beak(250,250);
   }
   
   function draw() {
@@ -16,27 +15,21 @@ function setup() {
   }
   
   function eyes(x,y) {
-    fill(colors[1]);
-  ellipse(x-50, y-50, 50, 35)
-  ellipse(x+50, y-50, 50, 35)
-  fill(colors[3])
-  circle(x-50, y-50, 30)
-  circle(x+50, y-50, 30)
+    fill("black");
+    arc(x+45, y-75, 80, 80, 0, PI - QUARTER_PI);
+    arc(x-45, y-75, 80, 80, QUARTER_PI, PI);
+  fill("yellow")
+  arc(x-45, y-60, 40, 40, PI/4, PI);
+  arc(x+45, y-60, 40, 40, 0, PI - QUARTER_PI)
    fill("black")
-  circle(x-50, y-50, 15)
-  circle(x+50, y-50, 15)
+  ellipse(x-45, y-50, 10,15)
+  ellipse(x+45, y-50, 10,15)
    fill("white")
-  circle(x-50, y-52, 5)
-  circle(x+50, y-52, 5)
+  circle(x-45, y-52, 5)
+  circle(x+45, y-52, 5)
   }
   
-  function beak(x,y){
-  fill("yellow")
-  arc(x, y, 150, 85, 0, PI )
-  line(x,y+25,x-75,y)
-  line(x+75,y,x-75,y)
-  line(x,y+25,x+75,y)
-  fill("black")
-  ellipse(x-25,y+10,5,10)
-  ellipse(x+25,y+10,5,10)
-  }
+  
+  
+ 
+  
